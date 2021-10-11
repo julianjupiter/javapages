ed8a4db2d1d74899a34cd4b8c4ed55c17adeb432
Julian Jupiter <julianjupiter.io@gmail.com>
2021-10-11 22:33:19 +0800

    Add reference page

---
+++ pages/reference/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Reference() {
+  return (
+    <div>
+      <Head>
+        <title>Reference</title>
+        <meta name="description" content="Reference" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h2>Reference</h2>
+      </main>
+    </div>
+  );
+}

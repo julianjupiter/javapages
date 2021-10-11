cd8bb80e20b3aa7d274cf2fd387040c89470c2c8
Julian Jupiter <julianjupiter.io@gmail.com>
2021-10-11 22:26:30 +0800

    Add news page

---
+++ pages/news.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function News() {
+  return (
+    <div>
+      <Head>
+        <title>News</title>
+        <meta name="description" content="News" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h2>News</h2>
+      </main>
+    </div>
+  );
+}

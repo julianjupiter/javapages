95456dcec741fae2f291e6c4f0d19d7c49465814
Julian Jupiter <julianjupiter.io@gmail.com>
2021-10-11 22:08:33 +0800

    Add download page for OpenJDK

---
+++ pages/download-openjdk.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Downlaod OpenJDK</title>
+        <meta name="description" content="Downlaod OpenJDK" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h2>Download OpenJDK</h2>
+      </main>
+    </div>
+  );
+}
